f265bcf4057c070243038c3b50ec81f0f9ec1d0b
Park Jee Hyun <jhyun19@gmail.com>
2016-07-06 16:32:13 +0900

    20160706

--- utils/AbnormalDetection.py
+++ utils/AbnormalDetection.py
@@ -1,3 +1,13 @@
 # -*- coding: utf-8 -*-
 
+from FileIO import Load
+from Similarity import Score
 
+class Solo:
+    def __init__(self):
+        pass
+
+
+class Neighbor:
+    def __init__(self):
+        pass
